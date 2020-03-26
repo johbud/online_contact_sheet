055295d118ea00dc8c04f3c050bb9e969478598e
johbud <john@johnbuddee.com>
2020-03-26 23:17:43 +0100

    hello world!

---
+++ application.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+app = Flas(__name__)
+
+@app_route('/', methods=["GET"])
+def index():
+    return "<h1>hello world!</h1>"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
