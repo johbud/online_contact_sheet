411c732cc58981eb6c2207319754a26a84308d7c
johbud <john@johnbuddee.com>
2020-03-26 23:43:31 +0100

    hello world!

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app_route('/', methods=["GET"])
+@app_route('/')
 def index():
     return "<h1>hello world!</h1>"
+
+if __name__ == '__main__':
+    app.run
